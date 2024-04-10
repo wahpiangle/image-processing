feff2eaf717b29447a017095ba8a123a1306ee92
Quan Ming <116425066+wahpiangle@users.noreply.github.com>
2024-04-10 14:15:29 +0800

    fix

--- grey_space_2.py
+++ grey_space_2.py
@@ -45,7 +45,7 @@ def sharpen_image_laplacian(image):
 
 def sharpen_image_kernel(image):
     # Define the sharpening kernel
-    kernel = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])
+    kernel = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]])
 
     sharpened_image = cv2.filter2D(image, -1, kernel)
     return sharpened_image
@@ -59,36 +59,33 @@ for key, image in imageMap.items():
     sharpened_image = sharpen_image_kernel(gamma_corrected_image)
 
     filtered_image = cv2.bilateralFilter(
-        sharpened_image, d=9, sigmaColor=120, sigmaSpace=120
+        sharpened_image, d=11, sigmaColor=120, sigmaSpace=120
     )
 
-    # # otsu
-    # filtered_image = np.uint8(filtered_image)
     _, thresh = cv2.threshold(
         filtered_image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU
     )
 
-    # Plot the thresholded image
-    # plt.figure(figsize=(6, 6))
-    # plt.imshow(thresh, cmap="gray")
-    # plt.title(f"{key} - Thresholded (OTSU)")
-    # plt.axis("off")
-    # plt.show()
     kernel = np.ones((5, 5), np.uint8)
-    opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations=0)
-    sure_bg = cv2.dilate(opening, kernel, iterations=9)
+    opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations=2)
+    sure_bg = cv2.dilate(opening, kernel, iterations=18)
     dist_transform = cv2.distanceTransform(opening, cv2.DIST_L2, 5)
-    _, sure_fg = cv2.threshold(dist_transform, 0.55 * dist_transform.max(), 255, 0)
+    _, sure_fg = cv2.threshold(dist_transform, 0.2 * dist_transform.max(), 255, 0)
     sure_fg = np.uint8(sure_fg)
     unknown = cv2.subtract(sure_bg, sure_fg)
     _, markers = cv2.connectedComponents(sure_fg)
     markers = markers + 1
     markers[unknown == 255] = 0
+
     segmented_image = cv2.watershed(image, markers)
     segmented_image[segmented_image == 1] = 0
     segmented_image[segmented_image != 0] = 255
 
-    final_result.append(markers)
+    # Ensure correct data type
+    segmented_image = np.uint8(segmented_image)
+    # get the binary image
+    segmented_image = cv2.bitwise_and(image, image, mask=segmented_image)
+    final_result.append(segmented_image)
 
 plt.figure(figsize=(12, 8))
 
