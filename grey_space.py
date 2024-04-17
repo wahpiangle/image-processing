8e8c1603b10f3f1743c01d65a405eaa4093fe944
chanyc110 <119403040+chanyc110@users.noreply.github.com>
2024-04-17 14:38:01 +0800

    update

--- grey_space.py
+++ grey_space.py
@@ -33,13 +33,13 @@ def preprocess_image(image):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     
     # Gaussian blur
-    # blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+    blurred = cv2.GaussianBlur(gray, (43,43 ), 0)
     
     # mean filtering
-    blurred = cv2.blur(gray, (5,5))
+    #blurred = cv2.blur(gray, (25,25))
     
     # bilateral filtering
-    # blurred = cv2.bilateralFilter(gray, 9, 75, 75)
+    #blurred = cv2.bilateralFilter(gray, 9, 255, 255)
     
     return blurred
 
@@ -52,24 +52,37 @@ def segment_flower_manual(image, threshold_value, max_value, threshold_type):
 def post_process(binary_image):
     # Morphological operations for noise removal and smoothing
     kernel = np.ones((5,5),np.uint8)
-
-    opened = cv2.morphologyEx(binary_image, cv2.MORPH_OPEN, kernel)
-    closed = cv2.morphologyEx(opened, cv2.MORPH_CLOSE, kernel)
-    # erosion = cv2.erode(binary_image,kernel,iterations = 3)
-    dilation = cv2.dilate(closed,kernel,iterations = 5)
+    kernel_ellipse = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
+    kernel_rectangular = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
+    kernel_cross = cv2.getStructuringElement(cv2.MORPH_CROSS, (5, 5))
     
-    return dilation 
+    
+    # erosion -> dilation -> close  BETTER 
+    erosion = cv2.erode(binary_image, kernel_ellipse, iterations=5)
+    open = cv2.morphologyEx(erosion, cv2.MORPH_OPEN, np.ones((5,5),np.uint8),iterations=3)
+    dilation = cv2.dilate(open, kernel, iterations=7)
+    dilation_2 = cv2.dilate(dilation, kernel_ellipse, iterations=3)
+    closed = cv2.morphologyEx(dilation_2, cv2.MORPH_CLOSE, np.ones((7,7),np.uint8), iterations=2)
+    
+    
+    
+    return closed
 
 def process_image(image):
     # Preprocessing
     preprocessed = preprocess_image(image)
     
+    
+    
     # Segmentation   last parameter = threshold type
     segmented = segment_flower_manual(preprocessed, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    #segmented = segment_flower_manual(edges, 100,255, cv2.THRESH_BINARY)
+    
     
     # Post-processing
     processed = post_process(segmented)
-    
+    # edge detection
+    #edges = cv2.Canny(processed, 50,50,3)
     return processed
 
 
@@ -81,7 +94,6 @@ for i, (name, img) in enumerate(imageMap.items()):
     row = i // 3
     col = i % 3
     axs[row, col].imshow(processed_img, cmap='gray')
-    axs[row, col].set_title(name + " Processed")
     
 plt.tight_layout()
 plt.show()
